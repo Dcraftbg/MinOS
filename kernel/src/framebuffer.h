c94388ddf502662d809222790b14163f6a43981f
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-08-02 18:39:02 +0300

    [framebuffer] Added todo for bitmap textures

--- kernel/src/framebuffer.h
+++ kernel/src/framebuffer.h
@@ -9,6 +9,14 @@ typedef struct {
     size_t height;
     size_t pitch_bytes;
 } Framebuffer;
+// TODO: Introduce:
+// typedef struct {
+//     uint8_t* data;
+//     size_t width;
+//     size_t height;
+//     size_t pitch_bits;
+// } Bitmap;
+// void fmbuf_draw_bitmap(Framebuffer* this, size_t left, size_t top, Bitmap bitmap, uint32_t color);
 // NOTE: assumes color is at MAX a 32 bit value.
 // If a framebuffer has a bpp = 1 that would indicate that any color value > 0 will be considered a 1 and any value equal to 0 a 0
 void fmbuf_draw_rect(Framebuffer* this, size_t left, size_t top, size_t right, size_t bottom, uint32_t color);
