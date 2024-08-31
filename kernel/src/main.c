fb9ba47d78d2906353aa9b821fe84c81441f99eb
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-08-31 12:46:22 +0300

    [rootfs] Refactored rootfs away into its own file

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -15,6 +15,7 @@
 #include "gdt.h"
 #include "exception.h"
 #include "vfs.h"
+#include "rootfs.h"
 #include "slab.h"
 #include "string.h"
 #include "task.h"
@@ -36,68 +37,6 @@ static void fbt() {
     if(buf.bpp != 32) return;
     fmbuf_fill(&buf, 0xFF212121);
 }
-
-#include "../embed.h"
-static void init_rootfs() {
-    intptr_t e = 0;
-    const char* path = NULL;
-    path = "/devices";
-    if((e = vfs_mkdir(path)) < 0) {
-        printf("ERROR: init_rootfs: Could not create %s : %s\n",path,status_str(e));
-        kabort();
-    }
-    for(size_t i = 0; i < embed_entries_count; ++i) {
-        EmbedEntry* entry = &embed_entries[i];
-        switch(entry->kind) {
-        case EMBED_FILE: {
-            if((e = vfs_create(entry->name)) < 0) {
-               printf("ERROR: init_rootfs: Could not create %s : %s\n",entry->name,status_str(e));
-               kabort();
-            }
-            VfsFile file = {0};
-            if((e = vfs_open(entry->name, &file, MODE_WRITE)) < 0) {
-                printf("ERROR: init_rootfs: Could not open %s : %s\n",entry->name,status_str(e));
-                kabort();
-            }
-            if((e = write_exact(&file, embed_data+entry->offset, entry->size)) < 0) {
-                printf("ERROR: init_rootfs: Could not write data: %s : %s\n",entry->name, status_str(e));
-                vfs_close(&file);
-                kabort();
-            }
-            vfs_close(&file);
-        } break;
-        case EMBED_DIR: {
-            if((e = vfs_mkdir(entry->name)) < 0) {
-                printf("ERROR: init_rootfs: Could not create %s : %s\n",entry->name,status_str(e));
-                kabort();
-            }
-        } break;
-        }
-    }
-#if 1
-    path = "/Welcome.txt";
-    if((e = vfs_create(path)) < 0) {
-        printf("ERROR: init_rootfs: Could not create %s : %s\n",path,status_str(e));
-        kabort();
-    }
-    {
-        const char* msg = "Hello and welcome to MinOS!\nThis is a mini operating system made in C.\nDate of compilation " __DATE__ ".";
-        VfsFile file = {0};
-        if((e = vfs_open(path, &file, MODE_WRITE)) < 0) {
-            printf("WARN: init_rootfs: Could not open %s : %s\n",path,status_str(e));
-            log_cache(kernel.inode_cache);
-            kabort();
-        } else {
-            if((e = write_exact(&file, msg, strlen(msg))) < 0) {
-                vfs_close(&file);
-                printf("WARN: init_rootfs: Could not write welcome message : %s\n",status_str(e));
-            }
-        } 
-        vfs_close(&file);
-    }
-#endif
-}
-
 
 static void dev_test() {
     const char* path = "/devices/tty0";
---
+++ kernel/src/rootfs.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "vfs.h"
+#include "print.h"
+#include "debug.h"
+#include "kernel.h"
+void init_rootfs();
