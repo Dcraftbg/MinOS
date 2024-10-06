1a612e322124a8a9b40e6480963c18342f8d0226
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-10-06 19:21:02 +0300

    [pic] Master first

--- kernel/src/pic.c
+++ kernel/src/pic.c
@@ -1,16 +1,16 @@
 #include "pic.h"
 void init_pic() {
-    outb(PIC2, 0x11);
     outb(PIC1, 0x11);
+    outb(PIC2, 0x11);
 
-    outb(PIC2_DATA, 0x28);
     outb(PIC1_DATA, 0x20);
+    outb(PIC2_DATA, 0x28);
 
-    outb(PIC2_DATA, 0x02);
     outb(PIC1_DATA, 0x04);
+    outb(PIC2_DATA, 0x02);
 
-    outb(PIC2_DATA, 0);
     outb(PIC1_DATA, 0);
+    outb(PIC2_DATA, 0);
 
     outb(PIC1_DATA, 0b11111111);
     outb(PIC2_DATA, 0b11111111);
