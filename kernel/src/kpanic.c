4e517f59a225e9061ced9396da4cb56fd25e4890
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-10-12 14:38:09 +0300

    [kpanic] Make kpanic variadic

--- kernel/src/kpanic.c
+++ kernel/src/kpanic.c
@@ -3,17 +3,31 @@
 #include "framebuffer.h"
 #include "fbwriter.h"
 #include "serial.h"
-void kpanic(const char* msg) {
-    serial_printstr(msg);
+#include "log.h"
+#include <stdarg.h>
+#include "print_base.h"
+
+void kpanic_fbwriter_write_sink(void* fbt_void, const char* str, size_t len) {
+    FbTextWriter* fbt = (FbTextWriter*)fbt_void;
+    fbwriter_draw_sized_str(fbt, str, len, 0xFFFFFF, 0xFF0000);
+}
+void kpanic(const char* fmt, ...) {
+    va_list va;
+    va_start(va, fmt);
+    kfatal_va(fmt, va);
+    va_end(va);
+
     Framebuffer fb = get_framebuffer_by_id(0);
     if(fb.addr) {
-        fmbuf_fill(&fb, 0xFF0000);
         FbTextWriter writer = {
             .fb = fb,
             .x = 0,
             .y = 0,
         };
         fbwriter_draw_cstr(&writer, "Kernel panic!\n", 0xFFFFFF, 0xFF0000);
-        fbwriter_draw_cstr(&writer, msg, 0xFFFFFF, 0xFF0000);
+        va_list va;
+        va_start(va, fmt);
+        print_base(&writer, kpanic_fbwriter_write_sink, fmt, va);
+        va_end(va);
     }
 }
--- kernel/src/kpanic.h
+++ kernel/src/kpanic.h
@@ -1,2 +1,3 @@
 #pragma once
-void kpanic(const char* msg);
+#include "utils.h"
+void kpanic(const char* fmt, ...) PRINTFLIKE(1, 2);
