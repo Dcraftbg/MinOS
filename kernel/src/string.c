53202f1776fc002ee5cadf567d1c98c9e8ea7685
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-10-12 13:28:55 +0300

    [string] Convertion functions

--- kernel/src/string.c
+++ kernel/src/string.c
@@ -73,3 +73,93 @@ size_t strlen(const char* cstr) {
     while(*head) head++;
     return head-cstr;
 }
+const char* strflip(char* str, size_t len) {
+    for(size_t i = 0; i < len/2; ++i) {
+        char c = str[i];
+        str[i] = str[len-i-1];
+        str[len-i-1] = c;
+    }
+    return str;
+}
+size_t itoa(char* buf, size_t cap, int value) {
+    size_t at=0;
+    if(value == 0 && at < cap) {
+        buf[at++] = '0';
+        return at;
+    }
+
+    if(value < 0 && at < cap) {
+        buf[at++] = '-';
+        value = -value;
+    }
+    char* whole = buf+at;
+    while(at < cap && value > 0) {
+        int k = value % 10;
+        value /= 10;
+        buf[at++] = '0'+k;
+    }
+    strflip(whole, (buf+at)-whole);
+    return at;
+}
+size_t sztoa(char* buf, size_t cap, size_t value) {
+    size_t at=0;
+    if(value == 0 && at < cap) {
+        buf[at++] = '0';
+        return at;
+    }
+    while(at < cap && value > 0) {
+        int k = value % 10;
+        value /= 10;
+        buf[at++] = '0'+k;
+    }
+    strflip(buf, at);
+    return at;
+}
+static const char* hex_digits = "0123456789ABCDEF";
+size_t utoha(char* buf, size_t cap, unsigned int value) {
+    size_t at=0;
+    while(at < cap && value > 0) {
+        int k = value & 0xF;
+        value >>= 4;
+        buf[at++] = hex_digits[k];
+    }
+    strflip(buf, at);
+    return at;
+}
+size_t uptrtoha_full(char* buf, size_t cap, uintptr_t value) {
+    size_t at=0;
+    while(at < cap && at < 16) {
+        int k = value & 0xF;
+        value >>= 4;
+        buf[at++] = hex_digits[k];
+    }
+    strflip(buf, at);
+    return at;
+}
+
+size_t atosz(const char* buf, const char** end) {
+    size_t value;
+    while(*buf && *buf >= '0' && *buf <= '9') {
+        value *= 10;
+        value += (*buf)-'0';
+        buf++;
+    }
+    *end = buf;
+    return value;
+}
+
+int atoi(const char* buf, const char** end) {
+    bool neg = false;
+    int value=0;
+    if(*buf == '-') {
+        neg = true;
+        buf++;
+    }
+    while(*buf && *buf >= '0' && *buf <= '9') {
+        value *= 10;
+        value += (*buf)-'0';
+        buf++;
+    }
+    *end = buf;
+    return neg ? -value : value;
+}
--- kernel/src/string.h
+++ kernel/src/string.h
@@ -8,3 +8,11 @@ int memcmp(void* s1, void* s2, size_t max);
 int strncmp(const char* s1, const char* s2, size_t max);
 int strcmp(const char *restrict s1, const char *restrict s2);
 size_t strlen(const char* cstr);
+const char* strflip(char* str, size_t len);
+size_t itoa(char* buf, size_t cap, int value);
+size_t sztoa(char* buf, size_t cap, size_t value);
+size_t utoha(char* buf, size_t cap, unsigned int value);
+size_t uptrtoha_full(char* buf, size_t cap, uintptr_t value);
+// If end=buf we clearly had a problem parsing this
+size_t atosz(const char* buf, const char** end);
+int atoi(const char* buf, const char** end);
