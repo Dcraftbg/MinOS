cd5b3964c4a8f8e27df82ed99945db46473a8857
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-10-13 11:59:33 +0300

    [prog_bar] Refactor out progress bar

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -31,6 +31,7 @@
 #include <minos/keycodes.h>
 #include <minos/key.h>
 #include <sync.h>
+#include "prog_bar.h"
 // TODO: create a symlink "/devices/keyboard" which will be a link to the currently selected keyboard
 // Like for example PS1 or USB or anything like that
 static void fbt() {
@@ -67,31 +68,6 @@ static void do_probe() {
 }
 #endif
 
-#include "fbwriter.h"
-#ifdef PROGRESS_BAR
-#define TOTAL_STEPS 23 
-static void update_bar(size_t at, const char* msg) {
-    Framebuffer fb = get_framebuffer_by_id(0);
-    if(fb.addr) {
-        size_t height = 10;
-        size_t width = (fb.width-30) * at / TOTAL_STEPS;
-        size_t x = 15;
-        size_t y = (fb.height-height-5); // (fb.height-height) / 2;
-        fmbuf_draw_rect(&fb, x, y, x+width, y+height, 0x00FF00);
-        size_t texoff = 20;
-        fmbuf_draw_rect(&fb, x, y-texoff, x+fb.width-15, y, 0);
-        FbTextWriter writer = {
-            .fb = fb,
-            .x = x,
-            .y = y-texoff,
-        };
-        fbwriter_draw_cstr_default(&writer, msg);
-    }
-}
-size_t step=0;
-#else
-#define update_bar(at, msg)
-#endif
 void _start() {
 
     disable_interrupts();
---
+++ kernel/src/prog_bar.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../../config.h"
+#ifdef PROGRESS_BAR
+#include <stddef.h>
+#include "fbwriter.h"
+#define TOTAL_STEPS 23 
+void update_bar(size_t at, const char* msg);
+extern size_t step;
+#else
+#define update_bar(at, msg)
+#endif
