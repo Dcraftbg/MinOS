6e61064a77ab9dc1aeafefb45f93c446ffcf36b6
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-10-18 19:53:11 +0300

    [fb] mmap

--- kernel/src/devices/fb/fb.c
+++ kernel/src/devices/fb/fb.c
@@ -2,6 +2,7 @@
 #include "../../log.h"
 #include <minos/fb/fb.h>
 #include "../../kernel.h"
+#include "../../mem/memregion.h"
 // GLOBALS
 Cache* fbd_cache=NULL;
 static FsOps fbOps = {0};
@@ -32,6 +33,50 @@ static intptr_t fb_ioctl(VfsFile* file, Iop op, void* arg) {
     return -INVALID_PARAM;
 }
 
+// NOTE: Assumes the framebuffer is one continious chunk of physical memory (which it is)
+// But in case this is ever an issue I'm putting this note here
+static intptr_t fb_mmap(VfsFile* file, MmapContext* context, void** addr, size_t size_pages) {
+    if(!file || !file->private || !context || !addr) return -INVALID_PARAM;
+    intptr_t e;
+    FbDevice* device = (FbDevice*)file->private;
+    size_t pages = PAGE_ALIGN_UP(device->fb.height*device->fb.pitch_bytes)/PAGE_SIZE;
+    if(size_pages != pages) return -SIZE_MISMATCH;
+    uint16_t rflags = 0;
+    pageflags_t pflags = KERNEL_PFLAG_USER | KERNEL_PTYPE_USER | KERNEL_PFLAG_PRESENT | KERNEL_PFLAG_EXEC_DISABLE;
+    if(file->mode & MODE_WRITE) {
+        pflags |= KERNEL_PFLAG_WRITE;
+        rflags |= MEMREG_WRITE;
+    }
+
+    MemoryRegion* region = memregion_new(
+        rflags,
+        pflags,
+        0, 0
+    ); 
+    if(!region) return -NOT_ENOUGH_MEM;
+    MemoryList* list = memlist_new(region);
+    if(!list) {
+        memregion_drop(region, NULL);
+        return -NOT_ENOUGH_MEM;
+    }
+    MemoryList* insert_into = memlist_find_available(context->memlist, region, pages, pages);
+    if(!insert_into) {
+        e=-NOT_ENOUGH_MEM;
+        goto err_no_insert_point;
+    }
+    *addr = (void*)region->address;
+    if(!page_mmap(context->page_table, virt_to_phys(kernel.pml4, (uintptr_t)device->fb.addr), region->address, pages, pflags)) {
+        e=-NOT_ENOUGH_MEM;
+        goto mmap_fail;
+    }
+    return pages;
+mmap_fail:
+    page_unmap(context->page_table, virt_to_phys(kernel.pml4, (uintptr_t)device->fb.addr), pages);
+err_no_insert_point:
+    memlist_dealloc(list, NULL);
+    return e;
+}
+
 static intptr_t fb_open(struct Device* this, VfsFile* file, fmode_t mode) {
     file->private = this->private;
     return 0;
@@ -43,6 +88,7 @@ static void fb_close(VfsFile* file) {
 static intptr_t fb_init() {
     memset(&fbOps, 0, sizeof(fbOps));
     fbOps.ioctl = fb_ioctl;
+    fbOps.mmap  = fb_mmap;
     fbOps.close = fb_close;
     fbd_cache = create_new_cache(sizeof(FbDevice), "FbDevice");
     if(!fbd_cache) return -NOT_ENOUGH_MEM;
