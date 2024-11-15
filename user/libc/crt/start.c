3d147e9b100683c96a9b3850f8ced5f0f7745331
DcraftBg <czl_obshta@abv.bg>
2024-11-15 11:15:23 +0200

    [crt] Push rbp to align stack

--- user/libc/crt/start.c
+++ user/libc/crt/start.c
@@ -3,6 +3,7 @@
 #include <environ.h>
 extern int main(int argc, const char** argv, int envc, const char** envv);
 void _start(int argc, const char** argv, int envc, const char** envv) {
+    asm volatile("push %rbp");
     _libc_internal_init_heap();
     _libc_init_environ(envv, envc);
     exit(main(argc, argv, envc, envv));
