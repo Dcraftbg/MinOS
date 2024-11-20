77bccd4f39a6199b2dc55c7e8d5a1f49e64ce5d1
DcraftBg <czl_obshta@abv.bg>
2024-11-20 10:51:49 +0200

    [exception] Force exceptions to go through serial

--- kernel/src/exception.c
+++ kernel/src/exception.c
@@ -26,6 +26,7 @@ const char* gpf_table[] = {
      "IDT"
 };
 void exception_handler(IDTEFrame* frame) {
+    kernel.logger = &serial_logger;
     if(frame->type == EXCEPTION_PAGE_FAULT) {
         kerror("Page fault at virtual address %p",(void*)frame->cr2);
     }
