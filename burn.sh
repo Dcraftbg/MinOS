c8cef8b0f83af1f23e778fdb37e9bf5bb20154c9
DcraftBg <czl_obshta@abv.bg>
2024-11-20 10:58:31 +0200

    [burn.sh] A quick to use burn.sh script

---
+++ burn.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+if [ -z "$BURN_ONTO" ]; then
+    echo "$0: Missing path to install onto (Please specify BURN_ONTO=)"
+    exit 1
+fi
+
+set -xe
+
+
+cp -v ./bin/OS.iso ./bin/OS_Bios.iso
+./kernel/vendor/limine/limine bios-install ./bin/OS_Bios.iso
+sudo dd if=./bin/OS_Bios.iso of=$BURN_ONTO status=progress oflag=sync
+sync
+sudo eject $BURN_ONTO
