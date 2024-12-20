98f9f1f241312a5676c656384aa6c0fff5575d5d
DcraftBg <czl_obshta@abv.bg>
2024-12-20 11:15:49 +0200

    [crt|libc] Dependency analysis on crt and libc

--- buildsys/build/crt.c
+++ buildsys/build/crt.c
@@ -3,7 +3,7 @@ bool build_crt0() {
     #define BINDIR LIBC_CRT_TARGET_DIR 
     #define SRCDIR "./user/libc/crt/"
     #define LIBDIR "./bin/std/"
-    if(!build_user_dir(SRCDIR, BINDIR, true)) return false; 
+    if(!build_user_dir(SRCDIR, BINDIR, false)) return false; 
     #undef BINDIR
     #undef SRCDIR
     #undef LIBDIR
--- buildsys/build/libc.c
+++ buildsys/build/libc.c
@@ -3,7 +3,7 @@ bool build_libc() {
     #define BINDIR LIBC_TARGET_DIR 
     #define SRCDIR "./user/libc/src/"
     #define LIBDIR "./bin/std/"
-    if(!build_user_dir(SRCDIR, BINDIR, true)) return false; 
+    if(!build_user_dir(SRCDIR, BINDIR, false)) return false; 
     #undef BINDIR
     #undef SRCDIR
     #undef LIBDIR
