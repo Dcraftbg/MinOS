66abd0f61c0324982a31407cccd1b471ee2ef8fe
DcraftBg <czl_obshta@abv.bg>
2024-12-29 14:19:56 +0200

    [user] Update install to make it more usr friendly
    If the user doesn't have the toolchain in his path the install script
    will inform them of missing gcc in their path and how to go about fixing
    it

--- user/dim/install.sh
+++ user/dim/install.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if ! command -v x86_64-minos-gcc &> /dev/null; then
+    echo "In order to build dim for MinOS you need the MinOS toolchain"
+    echo "For more information checkout <path to MinOS>/user/toolchain/build.sh"
+    echo "Make sure to add <path to MinOS>/user/toolchain/bin/binutils/bin/ to your PATH!"
+fi 
+
 if [ ! -d "dim" ]; then
     git clone https://github.com/Dcraftbg/dim.git
 fi
--- user/lua-5.4.7/install.sh
+++ user/lua-5.4.7/install.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if ! command -v x86_64-minos-gcc &> /dev/null; then
+    echo "In order to build lua for MinOS you need the MinOS toolchain"
+    echo "For more information checkout <path to MinOS>/user/toolchain/build.sh"
+    echo "Make sure to add <path to MinOS>/user/toolchain/bin/binutils/bin/ to your PATH!"
+fi 
+
 lua_version="5.4.7"
 if [ ! -f "lua-$lua_version.tar.gz" ]; then
     wget https://www.lua.org/ftp/lua-$(lua_version).tar.gz
