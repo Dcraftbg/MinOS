fab996bb93156de8652b926d00aee56e19455c83
DcraftBg <czl_obshta@abv.bg>
2025-01-03 22:29:24 +0200

    [interrupt] Fix interrupt

--- kernel/src/interrupt.c
+++ kernel/src/interrupt.c
@@ -6,8 +6,10 @@
 void irq_eoi(size_t irq) {
     pic_end(irq);
 }
+// TODO: I should do +0x20 because irq_register is NOT going to be used by exception definitions as they're platform specific
+// and it makes irq_eoi and irq_clear inconsistent
 void irq_register(size_t irq, void (*handler)(), irq_flags_t flags) {
-    idt_register(irq, handler, flags & IRQ_FLAG_FAST ? IDT_TRAP_TYPE : IDT_INTERRUPT_TYPE);
+    idt_register(irq, handler, flags & IRQ_FLAG_FAST ? IDT_INTERRUPT_TYPE : IDT_TRAP_TYPE);
 }
 void irq_clear(size_t irq) {
     pic_clear_mask(irq);
--- kernel/src/interrupt.h
+++ kernel/src/interrupt.h
@@ -3,7 +3,7 @@
 void irq_eoi(size_t irq);
 typedef int irq_flags_t;
 enum {
-    IRQ_FLAG_FAST
+    IRQ_FLAG_FAST = 0b1
 };
 void irq_register(size_t irq, void (*handler)(), irq_flags_t flags);
 void irq_clear(size_t irq);
