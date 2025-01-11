067ce87b8a90ae9d87e4e0b597337563ef1da215
DcraftBg <czl_obshta@abv.bg>
2025-01-11 23:58:30 +0200

    [libc/assert] Convert assert into expression

--- user/libc/include/assert.h
+++ user/libc/include/assert.h
@@ -4,10 +4,5 @@
 #define STRINGIFY2(x) #x
 #define STRINGIFY(x) STRINGIFY2(x)
 #define assert(x) \
-   do { \
-      if(!(x)) { \
-         fputs(__FILE__ ":" STRINGIFY(__LINE__) " Assertion failed: " STRINGIFY(x), stderr); \
-         exit(1);\
-      }\
-   } while(0)
+    x ? 0 : (fputs(__FILE__ ":" STRINGIFY(__LINE__) " Assertion failed: " STRINGIFY(x), stderr), exit(1))
 
