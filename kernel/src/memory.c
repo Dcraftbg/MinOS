73cc71765e1e77ff9afdaddad44c433f6964cdab
DcraftBg <czl_obshta@abv.bg>
2025-01-18 20:15:33 +0200

    [memory] kernel_pages_alloc|dealloc

--- kernel/src/memory.c
+++ kernel/src/memory.c
@@ -1,29 +1,34 @@
 #include "memory.h"
 #include "mem/bitmap.h"
 #include "kernel.h"
+paddr_t kernel_pages_alloc(size_t pages) {
+    mutex_lock(&kernel.map_lock);
+    void* addr = bitmap_alloc(&kernel.map, pages);
+    paddr_t res = (paddr_t)(((uintptr_t)addr) & (~KERNEL_MEMORY_MASK));
+    mutex_unlock(&kernel.map_lock);
+    return res;
+}
 paddr_t kernel_page_alloc() {
-   mutex_lock(&kernel.map_lock);
-   void* addr = bitmap_alloc(&kernel.map, 1);
-   paddr_t res = (paddr_t)(((uintptr_t)addr) & (~KERNEL_MEMORY_MASK));
-   mutex_unlock(&kernel.map_lock);
-   return res;
+    return kernel_pages_alloc(1);
+}
+void kernel_pages_dealloc(paddr_t page, size_t count) {
+    mutex_lock(&kernel.map_lock);
+    bitmap_dealloc(&kernel.map, (void*)page, count);
+    mutex_unlock(&kernel.map_lock);
 }
 void kernel_page_dealloc(paddr_t page) {
-   mutex_lock(&kernel.map_lock);
-   bitmap_dealloc(&kernel.map, (void*)page, 1);
-   mutex_unlock(&kernel.map_lock);
+    kernel_pages_dealloc(page, 1);
 }
-
 void* kernel_malloc(size_t size) {
-   mutex_lock(&kernel.map_lock);
-   void* addr = bitmap_alloc(&kernel.map, (size+(PAGE_SIZE-1))/PAGE_SIZE);
-   if(addr) addr = (void*)((uintptr_t)addr | KERNEL_MEMORY_MASK);
-   mutex_unlock(&kernel.map_lock);
-   return addr;
+    mutex_lock(&kernel.map_lock);
+    void* addr = bitmap_alloc(&kernel.map, (size+(PAGE_SIZE-1))/PAGE_SIZE);
+    if(addr) addr = (void*)((uintptr_t)addr | KERNEL_MEMORY_MASK);
+    mutex_unlock(&kernel.map_lock);
+    return addr;
 }
 void kernel_dealloc(void* data, size_t size) {
-   if(!data) return;
-   mutex_lock(&kernel.map_lock);
-   bitmap_dealloc(&kernel.map, (void*)((uintptr_t)data & (~KERNEL_MEMORY_MASK)),(size+(PAGE_SIZE-1))/PAGE_SIZE);
-   mutex_unlock(&kernel.map_lock);
+    if(!data) return;
+    mutex_lock(&kernel.map_lock);
+    bitmap_dealloc(&kernel.map, (void*)((uintptr_t)data & (~KERNEL_MEMORY_MASK)),(size+(PAGE_SIZE-1))/PAGE_SIZE);
+    mutex_unlock(&kernel.map_lock);
 }
--- kernel/src/memory.h
+++ kernel/src/memory.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "page.h"
+
+paddr_t kernel_pages_alloc(size_t count);
 paddr_t kernel_page_alloc();
 void kernel_page_dealloc(paddr_t page);
+void kernel_pages_dealloc(paddr_t page, size_t count);
 void* kernel_malloc(size_t size);
 void  kernel_dealloc(void* data, size_t size);
 
