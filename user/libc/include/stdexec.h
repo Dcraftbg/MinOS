acbba39b6b72c0a3924988d6420149de869377c3
DcraftBg <czl_obshta@abv.bg>
2025-02-05 17:54:55 +0200

    [stdexec] execvp

--- user/libc/include/stdexec.h
+++ user/libc/include/stdexec.h
@@ -4,3 +4,4 @@
 static intptr_t exec(const char* path, const char** argv, size_t argc) {
     return execve(path, argv, argc, (const char**)environ, __environ_size);
 }
+intptr_t execvp(const char* path, const char** argv, size_t argc);
---
+++ user/libc/src/stdexec.c
@@ -0,0 +1,33 @@
+#include <stdexec.h>
+#include <string.h>
+#include <minos/status.h>
+#include <stdio.h>
+intptr_t execvp(const char* exe_path, const char** argv, size_t argc) {
+    if(
+        (exe_path[0] == '/') ||
+        (exe_path[0] == '.' && exe_path[1] == '/') ||
+        (exe_path[0] == '.' && exe_path[1] == '.' && exe_path[2] == '/')
+    ) {
+        return exec(exe_path, argv, argc);
+    }
+    const char* path = getenv("PATH");
+    if(!path) return -NOT_FOUND;
+    char pathbuf[PATH_MAX];
+    size_t exe_path_len = strlen(exe_path);
+
+    char* end;
+    for(;*path; path = end + 1) {
+        end = strchr(path, ':');
+        if(!(*end)) break;
+        if(end == path) continue;
+        if((end-path) + 1 + exe_path_len >= sizeof(pathbuf)) continue;
+        strncpy(pathbuf, path, end-path);
+        if((end-1)[0] != '/') strcat(pathbuf, "/");
+        strcat(pathbuf, exe_path);
+        Stats stats;
+        if(stat(pathbuf, &stats) >= 0 && stats.kind == INODE_FILE) {
+            return exec(pathbuf, argv, argc);
+        }
+    }
+    return -NOT_FOUND;
+}
