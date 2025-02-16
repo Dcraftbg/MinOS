e0e45320bdabdca7e143553febec62e5ed9f2fc4
DcraftBg <czl_obshta@abv.bg>
2025-02-16 13:03:45 +0200

    [libc] inttypes.h PRI*

--- user/libc/include/inttypes.h
+++ user/libc/include/inttypes.h
@@ -1,2 +1,26 @@
 #pragma once
 #include <stdint.h>
+#define PRIu8  PRIu32
+#define PRIu16 PRIu32
+#define PRIu32 "u"
+#define PRIu64 "llu"
+
+#define PRIi8  PRIi32
+#define PRIi16 PRIi32
+#define PRIi32 "d"
+#define PRIi64 "lld"
+
+#define PRId8  PRIi8 
+#define PRId16 PRIi16
+#define PRId32 PRIi32
+#define PRId64 PRIi64
+
+#define PRIx8  "x"
+#define PRIx16 "x"
+#define PRIx32 "x"
+#define PRIx64 "x"
+
+#define PRIX8  "X"
+#define PRIX16 "X"
+#define PRIX32 "X"
+#define PRIX64 "X"
