3aa3c5444f0c744651fd3351bccc1fe6ce04620a
DcraftBg <czl_obshta@abv.bg>
2025-02-16 13:04:11 +0200

    [stdexec] environ inc using "" instead of <>

--- user/libc/include/stdexec.h
+++ user/libc/include/stdexec.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <minos/sysstd.h>
-#include <environ.h>
+#include "environ.h"
 static intptr_t exec(const char* path, const char** argv, size_t argc) {
     return execve(path, argv, argc, (const char**)environ, __environ_size);
 }
