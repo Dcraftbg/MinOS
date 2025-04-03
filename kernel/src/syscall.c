f1f35ceb4c5f1c48d76614d19996107c955e92f5
DcraftBg <czl_obshta@abv.bg>
2025-04-03 17:47:48 +0300

    [heap] HEAP_EXACT flag

--- kernel/src/syscall.c
+++ kernel/src/syscall.c
@@ -402,6 +402,10 @@ intptr_t sys_heap_create(uint64_t flags, void* addr, size_t size_min) {
         memlist_dealloc(list, NULL);
         return -NOT_ENOUGH_MEM;
     }
+    if((flags & HEAP_EXACT) && region->address != (uintptr_t)addr) {
+        memlist_dealloc(list, NULL);
+        return -VIRTUAL_SPACE_OCCUPIED;
+    }
     Heap* heap = heap_new(cur_proc->heapid++, region->address, region->pages, flags);
     if(!heap) {
         memlist_dealloc(list, NULL);
--- libs/std/include/minos/heap.h
+++ libs/std/include/minos/heap.h
@@ -1,6 +1,7 @@
 #pragma once
 enum {
     HEAP_RESIZABLE=0b1,
+    HEAP_EXACT=0b10,
 };
 typedef struct {
     void* address;
