629fef1d18fcddc776a2a95e378443ee93fb7179
DcraftBg <czl_obshta@abv.bg>
2025-04-13 17:40:55 +0300

    [hello|init|shell] rewrite to nob

--- buildsys/build/hello.c
+++ buildsys/build/hello.c
@@ -1,28 +1,6 @@
 bool build_hello() {
-    #define BINDIR "./bin/user/hello/"
-    #define SRCDIR "./user/hello/src/"
-    #define LIBDIR "./bin/std/"
-    if(!build_user_dir(SRCDIR, BINDIR, false)) return false; 
-    Nob_File_Paths paths = {0};
-    if(!find_objs(BINDIR, &paths)) {
-        nob_da_free(paths);
-        return false;
-    }
-    if(!find_libc(&paths, true)) {
-        nob_da_free(paths);
-        return false;
-    }
-    if(!find_objs(LIBDIR, &paths)) {
-        nob_da_free(paths);
-        return false;
-    }
-    if(!ld(&paths, BINDIR "hello"  , "./user/hello/link.ld")) {
-        nob_da_free(paths);
-        return false;
-    }
-    nob_da_free(paths);
-    #undef BINDIR
-    #undef SRCDIR
-    #undef LIBDIR
-    return true;
+    Nob_Cmd cmd = { 0 };
+    bool res = go_run_nob_inside(&cmd, "user/hello");
+    nob_cmd_free(cmd);
+    return res;
 }
--- buildsys/build/init.c
+++ buildsys/build/init.c
@@ -1,28 +1,6 @@
 bool build_init() {
-    #define BINDIR "./bin/user/init/"
-    #define SRCDIR "./user/init/src/"
-    #define LIBDIR "./bin/std/"
-    if(!build_user_dir(SRCDIR, BINDIR, false)) return false; 
-    Nob_File_Paths paths = {0};
-    if(!find_objs(BINDIR, &paths)) {
-        nob_da_free(paths);
-        return false;
-    }
-    if(!find_objs(LIBDIR, &paths)) {
-        nob_da_free(paths);
-        return false;
-    }
-    if(!find_libc(&paths, false)) {
-        nob_da_free(paths);
-        return false;
-    }
-    if(!ld(&paths, BINDIR "init"  , "./user/init/link.ld")) {
-        nob_da_free(paths);
-        return false;
-    }
-    nob_da_free(paths);
-    #undef BINDIR
-    #undef SRCDIR
-    #undef LIBDIR
-    return true;
+    Nob_Cmd cmd = { 0 };
+    bool res = go_run_nob_inside(&cmd, "user/init");
+    nob_cmd_free(cmd);
+    return res;
 }
