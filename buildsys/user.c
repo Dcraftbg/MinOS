984d8e73cd76da81e9b7a4d9dddcad3bb96589b5
DcraftBg <czl_obshta@abv.bg>
2025-04-13 16:23:26 +0300

    [fbtest] go_run_nob_inside

--- buildsys/user.c
+++ buildsys/user.c
@@ -45,3 +45,31 @@ bool build_user(const char* what) {
     }
     return true; 
 }
+// bool go_run_inside(Nob_Cmd* cmd, const char* dir) {
+//     const char* cur = nob_get_current_dir_temp();
+//     assert(cur);
+//     cur = nob_temp_realpath(cur);
+//     if(!nob_set_current_dir(dir)) return false;
+//     if(!nob_set_current_dir(dir)) return false;
+//     bool res = nob_cmd_run_sync_and_reset(cmd);
+//     assert(nob_set_current_dir(cur));
+//     return res;
+// }
+
+// TODO: cleanup temp on failure
+bool go_run_nob_inside(Nob_Cmd* cmd, const char* dir) {
+    size_t temp = nob_temp_save();
+    const char* cur = nob_get_current_dir_temp();
+    assert(cur);
+    cur = nob_temp_realpath(cur);
+    if(!nob_set_current_dir(dir)) return false;
+    if(nob_file_exists("nob") != 1) {
+        nob_cmd_append(cmd, NOB_REBUILD_URSELF("nob", "nob.c"));
+        if(!nob_cmd_run_sync_and_reset(cmd)) return false;
+    }
+    nob_cmd_append(cmd, "./nob");
+    bool res = nob_cmd_run_sync_and_reset(cmd);
+    assert(nob_set_current_dir(cur));
+    nob_temp_rewind(temp);
+    return res;
+}
