431e3442fbee44d3d5f4c15643d596bffa974f9a
DcraftBg <czl_obshta@abv.bg>
2025-04-13 17:09:25 +0300

    [ansi_test] Migrate to nob

--- buildsys/build/ansi_test.c
+++ buildsys/build/ansi_test.c
@@ -1,28 +1,6 @@
 bool build_ansi_test() {
-    #define BINDIR "./bin/user/ansi_test/"
-    #define SRCDIR "./user/ansi_test/src/"
-    #define LIBDIR "./bin/std/"
-    if(!build_user_dir(SRCDIR, BINDIR, false)) return false; 
-    Nob_File_Paths paths = {0};
-    if(!find_objs(BINDIR, &paths)) {
-        nob_da_free(paths);
-        return false;
-    }
-    if(!find_objs(LIBDIR, &paths)) {
-        nob_da_free(paths);
-        return false;
-    }
-    if(!find_libc(&paths, true)) {
-        nob_da_free(paths);
-        return false;
-    }
-    if(!ld(&paths, BINDIR "ansi_test"  , "./user/ansi_test/link.ld")) {
-        nob_da_free(paths);
-        return false;
-    }
-    nob_da_free(paths);
-    #undef BINDIR
-    #undef SRCDIR
-    #undef LIBDIR
-    return true;
+    Nob_Cmd cmd = { 0 };
+    bool res = go_run_nob_inside(&cmd, "user/ansi_test");
+    nob_cmd_free(cmd);
+    return res;
 }
