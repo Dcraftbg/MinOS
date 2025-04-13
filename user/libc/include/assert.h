39145b8a0a75218ac7f7165e298e6b89e6c146ea
DcraftBg <czl_obshta@abv.bg>
2025-04-13 19:08:49 +0300

    [assert] Prefix stringify with __ to fix redef

--- user/libc/include/assert.h
+++ user/libc/include/assert.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <stdlib.h>
 #include <stdio.h>
-#define STRINGIFY2(x) #x
-#define STRINGIFY(x) STRINGIFY2(x)
+#define __STRINGIFY2(x) #x
+#define __STRINGIFY(x) __STRINGIFY2(x)
 #define assert(x) \
-    x ? 0 : (fputs(__FILE__ ":" STRINGIFY(__LINE__) " Assertion failed: " STRINGIFY(x), stderr), exit(1))
+    x ? 0 : (fputs(__FILE__ ":" __STRINGIFY(__LINE__) " Assertion failed: " __STRINGIFY(x), stderr), exit(1))
 
