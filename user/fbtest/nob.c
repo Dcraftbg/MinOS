f956d2d720552f65970d36ff266188c992a9019f
DcraftBg <czl_obshta@abv.bg>
2025-04-13 16:22:12 +0300

    [fbtest] rewrite with nob

---
+++ user/fbtest/nob.c
@@ -0,0 +1,36 @@
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+
+#ifndef MINOS_ROOT
+#   define MINOS_ROOT "../../"
+#endif
+#define BUILD_DIR "bin/"
+#define EXE      "fbtest"
+#if 1
+#   define COPY_DIR MINOS_ROOT "initrd/"
+#endif
+
+#define c_compiler(cmd)     nob_cmd_append(cmd, "x86_64-minos-gcc")
+#define c_output(cmd, path) nob_cmd_append(cmd, "-o", path)
+#define c_flags(cmd)        nob_cmd_append(cmd, "-MMD", "-MP", "-Wall", "-Wextra")
+
+bool cc(Nob_Cmd* cmd) {
+    c_compiler(cmd);
+    c_flags(cmd);
+    nob_cmd_append(cmd, "src/main.c");
+    c_output(cmd, BUILD_DIR EXE);
+    return nob_cmd_run_sync_and_reset(cmd);
+}
+int main(int argc, char **argv) {
+    NOB_GO_REBUILD_URSELF(argc, argv);
+    Nob_Cmd cmd = { 0 };
+    const char* exe = nob_shift_args(&argc, &argv);
+    if(!nob_mkdir_if_not_exists(BUILD_DIR)) return 1;
+    if(nob_needs_rebuild1(BUILD_DIR EXE, "src/main.c") && !cc(&cmd)) return 1;
+#ifdef COPY_DIR
+    if(nob_file_exists(COPY_DIR) == 1 && nob_needs_rebuild1(COPY_DIR EXE, BUILD_DIR EXE) && !nob_copy_file(BUILD_DIR EXE, COPY_DIR EXE)) return 1;
+#endif
+    return 0;
+}
+
+
