90e85cfc4e42457eade05966c5e74da92bbfb026
DcraftBg <czl_obshta@abv.bg>
2025-04-26 10:01:39 +0300

    [locale] better looking stubs

--- user/libc/src/locale.c
+++ user/libc/src/locale.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 char *setlocale(int category, const char *locale) {
-    fprintf(stderr, "ERROR: Todo. setlocale");
+    fprintf(stderr, "%s:%d: (WARN) setlocale is a stub\n", __FILE__, __LINE__);
     exit(1);
 }
 struct lconv *localeconv(void) {
-    fprintf(stderr, "ERROR: Todo. localeconv");
+    fprintf(stderr, "%s:%d: localeconv is a stub\n", __FILE__, __LINE__);
     exit(1);
 }
