5d1c3e7fc6a3b34c4ef69a0339530683aad9979d
DcraftBg <czl_obshta@abv.bg>
2025-04-26 10:00:21 +0300

    [libc] make assert call abort instead of exit

--- user/libc/include/assert.h
+++ user/libc/include/assert.h
@@ -4,5 +4,5 @@
 #define __STRINGIFY2(x) #x
 #define __STRINGIFY(x) __STRINGIFY2(x)
 #define assert(x) \
-    x ? 0 : (fputs(__FILE__ ":" __STRINGIFY(__LINE__) " Assertion failed: " __STRINGIFY(x), stderr), exit(1))
+    x ? 0 : (fputs(__FILE__ ":" __STRINGIFY(__LINE__) " Assertion failed: " __STRINGIFY(x) "\n", stderr), abort())
 
