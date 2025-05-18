c96a37ba9d3a09de7c3e4047bae66979e713384f
DcraftBg <czl_obshta@abv.bg>
2025-05-18 08:33:55 +0300

    [bootstrap] Submodule update only limine

--- bootstrap.c
+++ bootstrap.c
@@ -192,7 +192,7 @@ bool bootstrap_config(Build* build) {
 }
 bool bootstrap_submodules() {
     Nob_Cmd cmd = {0};
-    nob_cmd_append(&cmd, "git", "submodule", "update", "--init", "--depth", "1");
+    nob_cmd_append(&cmd, "git", "submodule", "update", "--init", "--depth", "1", "kernel/vendor/limine");
     if (!nob_cmd_run_sync(cmd)) {
         nob_cmd_free(cmd);
         return false;
