2da3ab7d84838c1fb4bd04587d87487396ca42e6
DcraftBg <czl_obshta@abv.bg>
2025-06-01 17:39:28 +0300

    [hello_window] rewrite using libwm

--- user/hello_window/nob.c
+++ user/hello_window/nob.c
@@ -1,38 +1,39 @@
+#define NOB_STRIP_PREFIX
 #define NOB_IMPLEMENTATION
 #include "../nob.h"
 
-#ifndef MINOS_ROOT
-#   define MINOS_ROOT "../../"
-#endif
-#define EXE      "hello-window"
-#define BUILD_DIR MINOS_ROOT "bin/" EXE "/"
-#if 1
-#   define COPY_DIR MINOS_ROOT "initrd/user/"
-#endif
 
-#define c_compiler(cmd)     nob_cmd_append(cmd, "x86_64-minos-gcc")
-#define c_output(cmd, path) nob_cmd_append(cmd, "-o", path)
-#define c_flags(cmd)        nob_cmd_append(cmd, "-Wall", "-Wextra", "-MD")
-
-bool cc(Nob_Cmd* cmd) {
-    c_compiler(cmd);
-    c_flags(cmd);
-    nob_cmd_append(cmd, "src/main.c");
-    c_output(cmd, BUILD_DIR EXE);
-    return nob_cmd_run_sync_and_reset(cmd);
-}
+const char* sources[] = {
+    "src/main.c",
+};
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
-    Nob_Cmd cmd = { 0 };
-    const char* exe = nob_shift_args(&argc, &argv);
-    if(!nob_mkdir_if_not_exists_silent(BUILD_DIR)) return 1;
-    Nob_String_Builder sbuf = { 0 };
-    Nob_File_Paths path_buf = { 0 };
-    if(nob_c_needs_rebuild1(&sbuf, &path_buf, BUILD_DIR EXE, "src/main.c") && !cc(&cmd)) return 1;
-#ifdef COPY_DIR
-    if(nob_file_exists(COPY_DIR) == 1 && nob_needs_rebuild1(COPY_DIR EXE, BUILD_DIR EXE) && !nob_copy_file(BUILD_DIR EXE, COPY_DIR EXE)) return 1;
-#endif
-    return 0;
+    Cmd cmd = { 0 };
+    char* cc = getenv("CC");
+    if(!cc) cc = "cc";
+    char* bindir = getenv("BINDIR");
+    if(!bindir) bindir = "bin";
+    char* minos_root = getenv("MINOSROOT");
+    if(!minos_root) {
+        nob_log(NOB_ERROR, "Missing $MINOSROOT");
+        return 1;
+    }
+    if(!nob_mkdir_if_not_exists_silent(bindir)) return 1;
+    if(!nob_mkdir_if_not_exists_silent(temp_sprintf("%s/hello_window", bindir))) return 1;
+    const char* output = temp_sprintf("%s/hello_window/hello_window", bindir);
+    File_Paths pathb = { 0 };
+    String_Builder stb = { 0 };
+    if(nob_c_needs_rebuild(&stb, &pathb, output, sources, ARRAY_LEN(sources))) {
+        cmd_append(&cmd, cc, "-o", output);
+        cmd_append(&cmd, temp_sprintf("-L%s/bin/libwm", minos_root));
+        cmd_append(&cmd, "-lwm");
+        cmd_append(&cmd, "-I../libwm/include");
+        da_append_many(&cmd, sources, ARRAY_LEN(sources));
+        if(!nob_cmd_run_sync_and_reset(&cmd)) return 1;
+    }
+    char* rootdir = getenv("ROOTDIR");
+    if(rootdir && !copy_file(output, temp_sprintf("%s/user/hello_window", rootdir))) 
+        return 1;
 }
 
 
