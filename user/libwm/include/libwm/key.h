cff6a7d39ba885f7a4575ba82a27187193d856c2
DcraftBg <czl_obshta@abv.bg>
2025-06-30 18:16:48 +0300

    Fix bug :3

--- user/libwm/include/libwm/key.h
+++ user/libwm/include/libwm/key.h
@@ -3,4 +3,4 @@
 #define WM_GETKEYCODE(event) (event)->arg1
 
 #define WM_SETKEY(event, key)      ((event)->arg0 = (key))
-#define WM_SETKEYCODE(event, code) ((event)->arg0 = (code))
+#define WM_SETKEYCODE(event, code) ((event)->arg1 = (code))
