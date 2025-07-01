ced250a9a61c434b7e9597d4535c87d705d8fefc
DcraftBg <czl_obshta@abv.bg>
2025-07-01 21:25:17 +0300

    [user/nob.c] add miniterm to project list

--- user/nob.c
+++ user/nob.c
@@ -21,6 +21,7 @@ const char* projects[] = {
     "simpdump",
     // "tinycc",
     "wm",
+    "miniterm",
 };
 bool go_run_nob_inside(Nob_Cmd* cmd, const char* dir, const char** argv, size_t argc) {
     size_t temp = nob_temp_save();
