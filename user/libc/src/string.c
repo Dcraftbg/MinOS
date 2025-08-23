b2ee83bc1853ccd2d363b7b4e1f0cf4f4ba84fc2
DcraftBg <czl_obshta@abv.bg>
2025-08-23 19:54:51 +0300

    [string] fix strcasecmp

--- user/libc/src/string.c
+++ user/libc/src/string.c
@@ -85,7 +85,7 @@ int strcasecmp(const char *restrict s1, const char *restrict s2) {
         s1++;
         s2++;
    }
-   return ((unsigned char)*s1)-((unsigned char)*s2);
+   return tolower((unsigned char)*s1)-tolower((unsigned char)*s2);
 }
 
 int strncasecmp(const char* s1, const char* s2, size_t max) {
