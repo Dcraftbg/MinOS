0be1654d11b1c29eb085b33ce4baa14708cf4e57
DcraftBg <czl_obshta@abv.bg>
2025-09-04 20:15:15 +0300

    [wm] remove dependency on collections/list.h

--- user/wm/src/main.c
+++ user/wm/src/main.c
@@ -12,10 +12,10 @@
 #include <minos/status.h>
 #include <minos/fb/fb.h>
 #include <minos/mouse.h>
-#include <collections/list.h>
 #include "darray.h"
 #include <unistd.h>
 #include <fcntl.h>
+#include "list_head.h"
 
 #include <libwm.h>
 #include <libwm/tags.h>
@@ -117,7 +117,7 @@ typedef struct {
 } Image;
 typedef struct Client Client;
 typedef struct {
-    struct list list;
+    struct list_head list;
     char name[WINDOW_NAME_MAX];
     Rectangle rect;
     uint32_t* content;
@@ -147,7 +147,7 @@ typedef struct {
     size_t len, cap;
 } SHMRegions;
 typedef struct Client {
-    struct list list;
+    struct list_head list;
     int fd;
     Windows child_windows;
     SHMRegions shm_regions;
@@ -237,7 +237,7 @@ static Image icon_x    = { 0 }, icon_x_hover    = { 0 },
 static Window* moving_window = NULL;
 static int moving_window_dx = 0, moving_window_dy = 0;
 // (per workspace?)
-static struct list windows = { 0 };
+static struct list_head windows = { 0 };
 // ....
 static Framebuffer fb0;
 // Input state
@@ -398,7 +398,7 @@ static void window_redraw_region(const Framebuffer* fb, const Window* win, const
 static void redraw_region(const Framebuffer* fb, const Rectangle* rect) {
     if(rect->l == rect->r || rect->b == rect->t) return;
     wallpaper_redraw_region(fb, rect);
-    for(struct list* head = windows.next; head != &windows; head = head->next) {
+    for(struct list_head* head = windows.next; head != &windows; head = head->next) {
         const Window* win = (Window*)head;
         if(rect_collides(rect, &win->rect)) {
             Rectangle area = rect_collision_rect(rect, &win->rect);
@@ -615,7 +615,7 @@ void client_thread(void* client_void) {
             size_t width = (content.r-content.l);
             size_t height = (content.b-content.t);
             da_push(&client->child_windows, window);
-            list_insert(&window->list, &windows);
+            list_insert(&windows, &window->list);
             redraw_region(&fb0, &window->rect);
             flush_framebuffer(&fb0);
             window->child_index = client->child_windows.len-1;
@@ -827,7 +827,7 @@ void spawn_init_applets(void) {
         run(i, (char**)argv);
     }
 }
-static struct list clients;
+static struct list_head clients;
 intptr_t start_server(void) {
     int server = socket(AF_MINOS, SOCK_STREAM, 0);
     if(server < 0) {
@@ -860,7 +860,7 @@ intptr_t start_server(void) {
             close(client_fd);
             continue;
         }
-        list_insert(&client->list, &clients);
+        list_insert(&clients, &client->list);
         gtgo(client_thread, (void*)client);
     }
     return 0;
@@ -897,7 +897,7 @@ void handle_mouse_event(int what, int x, int y, int button) {
         break;
     case GUI_MOUSE_EVENT_DOWN: {
         Vec2size cursorp = cursor_point();
-        for(struct list* head = windows.next; head != &windows; head = head->next) {
+        for(struct list_head* head = windows.next; head != &windows; head = head->next) {
             Window* win = (Window*)head;
             if(rect_collide_point(&win->rect, &cursorp)) {
                 Rectangle content_rect = window_get_content_rect(win);
@@ -931,7 +931,7 @@ void handle_mouse_event(int what, int x, int y, int button) {
                     }
                 }
                 list_remove(&win->list);
-                list_append(&win->list, &windows);
+                list_append(&windows, &win->list);
                 // TODO: Optimise this to not redraw the entire window.
                 draw_window(&fb0, win);
                 draw_image(&fb0, &cursor, mouse_x, mouse_y);
@@ -1076,7 +1076,7 @@ void keyboard_thread(void*) {
     fill_rect(&fb0, &rect, 0xFF212121);
     flush_framebuffer(&fb0);
 
-    for(struct list* head = clients.next; head != &clients; head = head->next) {
+    for(struct list_head* head = clients.next; head != &clients; head = head->next) {
         Client* client = (Client*)head;
         close(client->fd);
     }
